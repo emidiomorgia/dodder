d43c66dd2cb28afab954d49193b7a02a2a3c9f0d
Emidio Morgia <emidio@morgia.info>
2019-07-07 10:52:34 +0200

    Added secured layout (in progress)

--- dotnetcoreDddAngularMicroservices/ClientApp/src/app/app.module.ts
+++ dotnetcoreDddAngularMicroservices/ClientApp/src/app/app.module.ts
@@ -5,9 +5,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PublicComponentComponent } from './layouts/public/public-component/public-component.component';
+import { PublicComponent } from './layouts/public/public/public.component';
+import { SecureComponent } from './layouts/secure/secure.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponentComponent,
+    PublicComponent,
+    SecureComponent
   ],
   imports: [
     BrowserModule,

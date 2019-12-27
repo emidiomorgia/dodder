6b0f246dc6d620b513ba36247731eec1842670ef
Emidio Morgia <emidio@morgia.info>
2019-12-27 17:43:44 +0100

    created apigateway, core service and deploy for IIS and jenkins

---
+++ deploy/deploy.batch.windows.iis.cmd
@@ -0,0 +1,12 @@
+cmd /c "cd ..\src\Web\Client && npm install" && (
+cmd /c "npm install -g @angular/cli" && (
+cmd /c "cd ..\src\Web\Client && ng test --watch=false" && (
+cmd /c "cd ..\src\Web\Client && ng build --prod=true" && (
+cmd /c "cd ..\src\Web\Server && dotnet restore" && (
+cmd /c "cd ..\src\Web\Server\wwwroot && del *.* /F /Q" && (
+cmd /c "cd ..\src\Web\Client\dist\Client && copy *.* ..\..\..\Server\wwwroot" && (
+cmd /c "cd ..\src\Web\Server && dotnet build"&& (
+cmd /c "cd ..\src\Web\Server && dotnet publish -c Release -o out" && (
+cmd /c "cd %windir%\system32\inetsrv && appcmd stop apppool /apppool.name:dodder-web" && (
+cmd /c "cd ..\src\Web\Server\out && xcopy *.* c:\inetpub\dodder-web /E /Y"&& (
+cmd /c "cd %windir%\system32\inetsrv && appcmd start apppool /apppool.name:dodder-web" ))))))))))
---
+++ deploy/deploy.jenkins.windows.iis.cmd
@@ -0,0 +1,19 @@
+SET repo_folder=dotnetcore-ddd-angular-microservices\
+SET apppool=dodder-web
+
+cmd /c "npm install -g rimraf" && (
+cmd /c "rimraf dotnetcore-ddd-angular-microservices" && (
+cmd /c "git clone https://github.com/emidiomorgia/dotnetcore-ddd-angular-microservices.git" && (
+cmd /c "cd %repo_folder%src\Web\Client && npm install" && (
+cmd /c "npm install -g @angular/cli" && (
+cmd /c "cd %repo_folder%src\Web\Client && ng test --watch=false" && (
+cmd /c "cd %repo_folder%src\Web\Client && ng build --prod=true" && (
+cmd /c "cd %repo_folder%src\Web\Server && dotnet restore" && (
+cmd /c "cd %repo_folder%src\Web\Server\wwwroot && del *.* /F /Q" && (
+cmd /c "cd %repo_folder%src\Web\Client\dist\Client && copy *.* ..\..\..\Server\wwwroot" && (
+cmd /c "cd %repo_folder%src\Web\Server && dotnet build"&& (
+cmd /c "cd %repo_folder%src\Web\Server && dotnet publish -c Release -o out" && (
+cmd /c "cd C:\Windows\system32\inetsrv && appcmd stop apppool /apppool.name:%apppool%" && (
+cmd /c "cd %repo_folder%src\Web\Server\out && xcopy *.* c:\inetpub\%apppool% /E /Y"&& (
+cmd /c "cd %windir%\system32\inetsrv && appcmd start apppool /apppool.name:%apppool%" && (
+cmd /c "rimraf dotnetcore-ddd-angular-microservices" )))))))))))))))
